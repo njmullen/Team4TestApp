955d268054da3bb5ca3f09d85925ee226948fc0f
Nick Mullen <mullennj@gmail.com>
2018-04-01 09:32:55 -0400

    main gui

--- app/src/main/java/edu/pitt/cs1699/team4testapp/MainActivity.java
+++ app/src/main/java/edu/pitt/cs1699/team4testapp/MainActivity.java
@@ -2,6 +2,7 @@ package edu.pitt.cs1699.team4testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onBroadcastClick(View view){
+
+    }
+
+    public void onBuyStockClick(View view){
+
+    }
+
+    public void onSellStockClick(View view){
+
+    }
+
+    public void onStartServiceClick(View view){
+
+    }
+
 }
